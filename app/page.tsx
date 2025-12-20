c4ad1298e685691f4332a2df644153d17b9fbf45
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2025-12-20 15:08:18 +0500

    fix: Remove gap between NavBar and Features section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Features from "./components/Features";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex">
       <Features />
     </main>
   );
